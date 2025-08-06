3d6e84eeb53f456c0ca733263783d16aee5db4ee
Rodrigo Rahman <rodrigorahman@gmail.com>
2025-08-06 16:00:28 -0300

    Configuração Env

---
+++ lib/cinebox_main_app.dart
@@ -0,0 +1,14 @@
+import 'package:cinebox/ui/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CineboxMainApp extends StatelessWidget {
+  const CineboxMainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CineBox',
+      routes: {'/': (_) => SplashScreen()},
+    );
+  }
+}
---
+++ lib/ui/splash/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:cinebox/config/env.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash  Screen Riverpod'),
+      ),
+      body: Center(
+        child: Text(Env.googleApiKey),
+      ),
+    );
+  }
+}
